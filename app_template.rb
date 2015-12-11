e452081aac5816f66b1626f13fb279f3b4db1184
Rob Curtis <surreymagpie@yahoo.co.uk>
2015-12-11 22:22:21 +0000

    Add gems to Gemfile and initialise git repo

--- app_template.rb
+++ app_template.rb
@@ -12,3 +12,40 @@
 
 run 'rm README.rdoc && touch README.md'
 append_file 'README.md', "# #{app_name.humanize}\n\nTODO..."
+
+# ==========================================================
+# Set up commonly used gems
+# ==========================================================
+
+gem 'haml-rails'
+gem 'puma'
+gem 'faker'
+
+gem_group :production do
+  gem 'rails_12factor'
+end
+
+gem_group :development do
+  gem 'annotate'
+  gem 'awesome_print', require: 'ap'
+  gem 'better_errors'
+  gem 'quiet_assets'
+end
+
+gem_group :development, :test do
+  gem 'guard-rspec', require: false
+  gem 'rb-inotify', require: false
+  gem 'rspec-rails', require: false
+end
+
+gem_group :test do
+  gem 'capybara'
+  gem 'database_cleaner'
+  gem 'factory_girl_rails'
+  gem 'spring-commands-rspec'
+end
+
+run 'bundle install'
+git :init
+git add: '.'
+git commit: "-m 'Initial commit'"
